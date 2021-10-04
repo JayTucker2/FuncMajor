df44825d4e5d01789a996d22030df9b82a3517fb
JayTucker2 <prosnipa1221@gmail.com>
2021-10-04 08:48:38 -0500

    Cleaning up Func

--- FuncMajor/ViewController.swift
+++ FuncMajor/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+var str = ""
+var letter = ""
+class ViewController: UIViewController, UITextFieldDelegate{
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var ansLab: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        func grade(_ g: Double)-> String{
+            let grd = g
+            if grd > 59{
+                 str = "Passed"
+            }
+            else {
+                 str = "Failed"
+            }
+           return str
+        }
+       /* func letGrade(_ g: Double)-> String{
+            let grd = g
+            if grd >= 90{
+                letter = "A"
+            }
+            return letter
+        }*/
     }
-
-
+    @IBAction func ansBut(_ sender: Any) {
+        let result = str
+        ansLab.text = result
+    }
+    
 }
 
